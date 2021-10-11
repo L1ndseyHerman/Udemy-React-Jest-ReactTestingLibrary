19257400e46b28dc89eca3d0a6511402ae964c21
L1ndseyHerman <lherman21@jcu.edu>
2021-10-11 12:26:00 -0400

    Caught up to vid386: more tests, some of which depend on a button being clicked.

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+//  For the button click:
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 //  This is for testing suites (groups): Like test(), it doesn't require any imports.
@@ -26,5 +28,46 @@ describe("Greeting component", () => {
     //    For a negation:
     //expect(helloWorldElement).not.toBeInTheDocument();
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    //    Arrange:
+    render(<Greeting />);
+
+    //  Act:
+    //  This only works if there's only one button in the component.
+    //  Remember the classnames u had to add just for unit tests?
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //  Assert:
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    //    Arrange:
+    render(<Greeting />);
+
+    //  Act:
+    //  This only works if there's only one button in the component.
+    //  Remember the classnames u had to add just for unit tests?
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //  Assert:
+    //  Says to use queryByText() instead of getByText():
+    //  Returns null if it's not found, so that's why expecting it toBeNull();
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
